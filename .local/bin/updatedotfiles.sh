4496ccb1e12c62a090a725d17b7e4440b2aa53fc
Tifan Dwi Avianto <ifan.anvity@gmail.com>
2019-06-02 22:12:16 +0700

    Trying out things

--- .local/bin/updatedotfiles.sh
+++ .local/bin/updatedotfiles.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-git clone -q https://github.com/ifananvity/dotfiles.git
-mv -f dotfiles/.* $HOME
-rm -rf dotfiles
+git clone -q https://github.com/ifananvity/dotfiles.git > /dev/null 2>&1
+for dot in $(ls dotfiles/ -a); do
+	[ ! $dot = ".." ] && [ ! $dot = "." ] && mv -f dotfiles/$dot $HOME
+done && rm -rf dotfiles/
