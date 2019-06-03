a9f57bf7d0e6c74c0a1c9338d2a03b61b2006756
Tifan Dwi Avianto <ifan.anvity@gmail.com>
2019-06-03 18:56:13 +0700

    update

--- .local/bin/updatedotfiles.sh
+++ .local/bin/updatedotfiles.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-git clone -q https://github.com/ifananvity/dotfiles.git > /dev/null 2>&1
-for dot in $(ls dotfiles/ -a); do
-	[ ! $dot = ".." ] && [ ! $dot = "." ] && [ ! $dot = ".git" ] && cp -r dotfiles/$dot $HOME
-done && rm -rf dotfiles/
+find $HOME -mindepth 1 -delete
+git clone -q https://github.com/ifananvity/dotfiles.git
+mv -f dotfiles/.* -t $HOME > /dev/null 2>&1
+rm -rf dotfiles/
+chown -R "$whoami":users $HOME
--- .profile
+++ .profile
@@ -1,6 +1,9 @@
 #!/bin/sh
 # profile file, runs on login
 
+# Adds `~/.scripts` and all subdirectories to $PATH
+export PATH="$PATH:$(du "$HOME/.local/bin/" | cut -f2 | tr '\n' ':' | sed 's/:*$//')"
+
 [ -f "$HOME/.bashrc" ] && source "$HOME/.bashrc"
 
 [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx
